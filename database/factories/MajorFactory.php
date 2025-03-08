016f1baec7a057eb8bcbb349e49c26535747f470
Todor Mitevski <todormitevski5@gmail.com>
2025-03-08 20:58:36 +0100

    Add Major factory, seeder, controller

---
+++ database/factories/MajorFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Major;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Major>
+ */
+class MajorFactory extends Factory
+{
+    protected $model = Major::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->unique()->word . ' Engineering',
+        ];
+    }
+}

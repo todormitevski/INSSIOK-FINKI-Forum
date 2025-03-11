ebaf452719425a5e457cdd26451b22fc9c38870f
Todor Mitevski <todormitevski5@gmail.com>
2025-03-11 19:44:25 +0100

    Add initial resources for admin panel

---
+++ app/Filament/Resources/MajorResource/Pages/EditMajor.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament\Resources\MajorResource\Pages;
+
+use App\Filament\Resources\MajorResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditMajor extends EditRecord
+{
+    protected static string $resource = MajorResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\ViewAction::make(),
+            Actions\DeleteAction::make(),
+        ];
+    }
+}

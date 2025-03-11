5cb17df54e17db2fd06062a77110aa5e093723d7
slavkovskis <slavkovskis@yahoo.com>
2025-03-11 19:40:56 +0100

    Added some TODO comments

--- database/migrations/2025_02_10_200808_create_posts_table.php
+++ database/migrations/2025_02_10_200808_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('content');
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); //TODO: set null
             $table->foreignId('subject_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
--- database/migrations/2025_02_10_200816_create_comments_table.php
+++ database/migrations/2025_02_10_200816_create_comments_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->text('content');
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); //TODO: set null
             $table->foreignId('post_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
